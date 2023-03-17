92231957f69c5a6432aab7b1184d04eb1b20c473
Joao Victor <joaovsg@id.uff.br>
2023-03-17 19:16:04 -0300

    Changing the validate to not use yup

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -41,6 +41,25 @@ export default class ProductB extends Entity implements ProductInterface {
   }
 
   validate() {
-    ProductValidatorFactory.create().validate(this);
+    if(this.id.length === 0){
+      this.notification.addError({
+        context: "product",
+        message: "Id is required",
+      });
+    }
+
+    if(this._name.length === 0){
+      this.notification.addError({
+        context: "product",
+        message: "Name is required",
+      });
+    }
+
+    if(this._price < 0){
+      this.notification.addError({
+        context: "product",
+        message: "price must be greater than or equal to 0",
+      });
+    }
   }
 }
